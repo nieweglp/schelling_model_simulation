ee721194c37032de4d65a702e776b0fdc6d7f3e7
Paweł Niewęgłowski <pawel.nieweglowski@yrnet.com>
2020-04-30 22:06:25 +0200

    development of simulation....

--- simulation.py
+++ simulation.py
@@ -7,97 +7,92 @@ Created on Sat Mar 21 12:20:19 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
-#import time
 
 
-people = 100
-world = 10
-threshold = 0.5
+def initialize_world(people, world, population_split):
+    population_postitions_1 = []
+    population_postitions_2 = []
+    slice_population = int(people*population_split)
+    for i in range(slice_population):
+        population_postitions_1.append([np.random.randint(0, world),
+                              np.random.randint(0, world)])
+    for i in range(people-slice_population):
+        population_postitions_2.append([np.random.randint(0, world),
+                              np.random.randint(0, world)])
 
-
-def initialize_world(people, world):
-	population_postitions_1 = []
-	population_postitions_2 = []
-	slice_population = int(people/2)
-	for i in range(slice_population):
-		population_postitions_1.append([np.random.randint(0, world),
-						      np.random.randint(0, world)])
-		population_postitions_2.append([np.random.randint(0, world),
-						      np.random.randint(0, world)])
-	
-	world_map = np.zeros((world, world), dtype='int')
-	for loc in population_postitions_1:
-		world_map[loc[0], loc[1]] = 1
-	for loc in population_postitions_2:
-		world_map[loc[0], loc[1]] = -1
-		
-	return world_map
-
-world_map = initialize_world(people, world)
+    world_map = np.zeros((world, world), dtype='int')
+    for loc in population_postitions_1:
+        world_map[loc[0], loc[1]] = 1
+    for loc in population_postitions_2:
+        world_map[loc[0], loc[1]] = -1
+    return world_map
 
 def check_local_area(local_people):
-	local_people=list(local_people)
-	local_area_ratio={}
-	local_area_ratio['population_first_ratio'] = local_people.count(1)/len(local_people)
-	local_area_ratio['population_second_ratio'] = local_people.count(-1)/len(local_people)
-	local_area_ratio['neutral_ratio'] = local_people.count(0)/len(local_people)
-	return local_area_ratio
-
-new_world = world_map
+    local_people=list(local_people)
+    local_area_ratio={}
+    local_area_ratio['population_first_ratio'] = local_people.count(1)/len(local_people)
+    local_area_ratio['population_second_ratio'] = local_people.count(-1)/len(local_people)
+    local_area_ratio['neutral_ratio'] = local_people.count(0)/len(local_people)
+    return local_area_ratio
 
 
-nr, nc = np.where(world_map == 0)
-neutral_space = [[nr[i],nc[i]] for i in range(0, world_map.shape[0])]
+def find_new_place(neutral_space):
+    rand_new_place = np.random.choice(range(len(neutral_space)))
+    return neutral_space[rand_new_place]
 
-def find_new_place():
-	rand_new_place = np.random.choice(range(len(neutral_space)))
-	return neutral_space[rand_new_place]
+def move_on(world_map):
+    new_world = world_map.copy()
+    #temp_map = np.zeros((world, world), dtype='int')
+    nr, nc = np.where(world_map == 0)
+    neutral_space = [[nr[i],nc[i]] for i in range(0, world_map.shape[0])]
+    for r in range(0, world_map.shape[0]-1):
+        for c in range(0, world_map.shape[1]-1):
+            if world_map[r][c] == 1:
+                if check_local_area([world_map[r][c+1], world_map[r+1][c+1],
+                                    world_map[r+1][c],
+                                    world_map[r+1,c-1],
+                                    world_map[r][c-1],
+                                    world_map[r-1][c-1],
+                                    world_map[r-1][c],
+                                    world_map[r-1][c+1]])['population_first_ratio'] < threshold:
+                    rn, cn = find_new_place(neutral_space)
+                    neutral_space.remove([rn, cn])
+                    neutral_space.append([r, c])
+                    new_world[rn][cn] = 1
+            if world_map[r][c] == -1:
+                if check_local_area([world_map[r][c+1],
+                                world_map[r+1][c+1],
+                                world_map[r+1][c],
+                                world_map[r+1,c-1],
+                                world_map[r][c-1],
+                                world_map[r-1][c-1],
+                                world_map[r-1][c],
+                                world_map[r-1][c+1]])['population_second_ratio'] < threshold:
+                    rn, cn = find_new_place(neutral_space)
+                    neutral_space.remove([rn, cn])
+                    neutral_space.append([r, c])
+                    new_world[rn][cn] = -1
+    return new_world
+
+people = 10
+world = 100
+threshold = 0.3
+population_split = 0.2
 
 
-temp_map = np.zeros((world, world), dtype='int')
-for r in range(0, world_map.shape[0]-1)
-	for c in range(0, world_map.shape[1]-1):
-		temp_map[r][c] = check_local_area([world_map[r][c+1], world_map[r+1][c+1], world_map[r+1][c], world_map[r+1,c-1], world_map[r][c-1], world_map[r-1][c-1], world_map[r-1][c], world_map[r-1][c+1]])['population_first_ratio']
-		if world_map[r][c] == 1:
-			if check_local_area([world_map[r][c+1], world_map[r+1][c+1], world_map[r+1][c],
-			     world_map[r+1,c-1], world_map[r][c-1], world_map[r-1][c-1],
-			        world_map[r-1][c], world_map[r-1][c+1]])['population_first_ratio'] < threshold:
-				rn, cn = find_new_place()
-				neutral_space.remove([rn, cn])
-				neutral_space.append([r, c])
-				new_world[rn][cn] = 1
-		if world_map[r][c] == -1:
-			if check_local_area([world_map[r][c+1], 
-							world_map[r+1][c+1], 
-							world_map[r+1][c],
-							world_map[r+1,c-1], 
-							world_map[r][c-1], 
-							world_map[r-1][c-1],
-							world_map[r-1][c], 
-							world_map[r-1][c+1]])['population_second_ratio'] < threshold:
-				rn, cn = find_new_place()
-				neutral_space.remove([rn, cn])
-				neutral_space.append([r, c])
-				new_world[rn][cn] = -1
-
-
-world_map == new_world
-			
-
-
-world_map[-1][0]
-	
-		
-elif: world_map[r][c] == -1:
-	...
-else:
-	continue
-	
+world_map = initialize_world(people, world, population_split)
 fig, ax = plt.subplots()
-ax.cla()
-ax.imshow(world_map, cmap='coolwarm')
-ax.set_title('People in population: {}'.format(people*2))
-#	plt.pause(0.1)
+
+for i in range(10):
+    world_map = move_on(world_map)
+    ax.cla()
+    ax.imshow(world_map, cmap='coolwarm')
+    ax.set_title('Iteration: {}'.format(i))
+    plt.pause(0.2)
+
+
+
+#
 
 
 
